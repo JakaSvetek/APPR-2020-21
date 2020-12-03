8112d887051e272c0ebb8934d4bbeadb931f2831
Jaka Svetek <jack.svetek@gmail.com>
2020-12-03 16:09:38 +0100

    Zacetek uvoza podatkov

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -1,55 +1,46 @@
-# 2. faza: Uvoz podatkov
+# UVOZ PODATKOV
 
-sl <- locale("sl", decimal_mark=",", grouping_mark=".")
+library(dplyr)
+library(tidyr)
+library(readr)
 
-# Funkcija, ki uvozi občine iz Wikipedije
-uvozi.obcine <- function() {
-  link <- "http://sl.wikipedia.org/wiki/Seznam_ob%C4%8Din_v_Sloveniji"
-  stran <- html_session(link) %>% read_html()
-  tabela <- stran %>% html_nodes(xpath="//table[@class='wikitable sortable']") %>%
-    .[[1]] %>% html_table(dec=",")
-  for (i in 1:ncol(tabela)) {
-    if (is.character(tabela[[i]])) {
-      Encoding(tabela[[i]]) <- "UTF-8"
-    }
-  }
-  colnames(tabela) <- c("obcina", "povrsina", "prebivalci", "gostota", "naselja",
-                        "ustanovitev", "pokrajina", "regija", "odcepitev")
-  tabela$obcina <- gsub("Slovenskih", "Slov.", tabela$obcina)
-  tabela$obcina[tabela$obcina == "Kanal ob Soči"] <- "Kanal"
-  tabela$obcina[tabela$obcina == "Loški potok"] <- "Loški Potok"
-  for (col in c("povrsina", "prebivalci", "gostota", "naselja", "ustanovitev")) {
-    if (is.character(tabela[[col]])) {
-      tabela[[col]] <- parse_number(tabela[[col]], na="-", locale=sl)
-    }
-  }
-  for (col in c("obcina", "pokrajina", "regija")) {
-    tabela[[col]] <- factor(tabela[[col]])
-  }
-  return(tabela)
-}
+stolpci <- c("Name", "Country", "Birth_date", "Time", "Points", "Swim_date", "Meet_name", "Meet_city", "Meet_country")
 
-# Funkcija, ki uvozi podatke iz datoteke druzine.csv
-uvozi.druzine <- function(obcine) {
-  data <- read_csv2("podatki/druzine.csv", col_names=c("obcina", 1:4),
-                    locale=locale(encoding="Windows-1250"))
-  data$obcina <- data$obcina %>% strapplyc("^([^/]*)") %>% unlist() %>%
-    strapplyc("([^ ]+)") %>% sapply(paste, collapse=" ") %>% unlist()
-  data$obcina[data$obcina == "Sveti Jurij"] <- iconv("Sveti Jurij ob Ščavnici", to="UTF-8")
-  data <- data %>% pivot_longer(`1`:`4`, names_to="velikost.druzine", values_to="stevilo.druzin")
-  data$velikost.druzine <- parse_number(data$velikost.druzine)
-  data$obcina <- parse_factor(data$obcina, levels=obcine)
-  return(data)
-}
 
-# Zapišimo podatke v razpredelnico obcine
-obcine <- uvozi.obcine()
 
-# Zapišimo podatke v razpredelnico druzine.
-druzine <- uvozi.druzine(levels(obcine$obcina))
+SCM50 <- read.csv("podatki/50m_SCM.csv", encoding = "UTF-8") %>%
+  select(1, 2, 3, 6, 7, 9, 14, 15, 16) %>%
+  .[c(5, 4, 6, 2, 7, 3, 1, 8, 9)]
+colnames(SCM50) <- stolpci
 
-# Če bi imeli več funkcij za uvoz in nekaterih npr. še ne bi
-# potrebovali v 3. fazi, bi bilo smiselno funkcije dati v svojo
-# datoteko, tukaj pa bi klicali tiste, ki jih potrebujemo v
-# 2. fazi. Seveda bi morali ustrezno datoteko uvoziti v prihodnjih
-# fazah.
+
+SCM100 <- read.csv("podatki/100m_SCM.csv", encoding = "UTF-8") %>%
+  select(1, 2, 3, 6, 7, 9, 14, 15, 16) %>%
+  .[c(5, 4, 6, 2, 7, 3, 1, 8, 9)]
+colnames(SCM100) <- stolpci
+
+
+SCM200 <- read.csv("podatki/200m_SCM.csv", encoding = "UTF-8") %>%
+  select(1, 2, 3, 6, 7, 9, 14, 15, 16) %>%
+  .[c(5, 4, 6, 2, 7, 3, 1, 8, 9)]
+colnames(SCM200) <- stolpci
+
+
+LCM50 <- read.csv("podatki/50m_LCM.csv", encoding = "UTF-8") %>%
+  select(1, 2, 3, 6, 7, 9, 14, 15, 16) %>%
+  .[c(5, 4, 6, 2, 7, 3, 1, 8, 9)]
+colnames(LCM50) <- stolpci
+
+
+LCM100 <- read.csv("podatki/100m_LCM.csv", encoding = "UTF-8") %>%
+  select(1, 2, 3, 6, 7, 9, 14, 15, 16) %>%
+  .[c(5, 4, 6, 2, 7, 3, 1, 8, 9)]
+colnames(LCM100) <- stolpci
+
+
+LCM200 <- read.csv("podatki/200m_LCM.csv", encoding = "UTF-8") %>%
+  select(1, 2, 3, 6, 7, 9, 14, 15, 16) %>%
+  .[c(5, 4, 6, 2, 7, 3, 1, 8, 9)]
+colnames(LCM200) <- stolpci
+
+
