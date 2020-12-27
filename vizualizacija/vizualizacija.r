1fee59cf822a330d9cbdfe10988e084ff02b29b3
Jaka Svetek <jack.svetek@gmail.com>
2020-12-27 15:10:50 +0100

    Naredil funkcijo za graf napredka.

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,15 +1,11 @@
 # 3. faza: Vizualizacija podatkov
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-# Če zemljevid nima nastavljene projekcije, jo ročno določimo
-proj4string(zemljevid) <- CRS("+proj=utm +zone=10+datum=WGS84")
+require(ggplot2)
 
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
+# Uvozim podatke
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+Graf_napredek <- function(Razdalja, Bazen)
+  ggplot(filter(podatki, Distance == Razdalja, Pool == Bazen), aes(x=Swim_date, y=Time)) +
+  geom_line() + 
+  ylab("Swim Time") + 
+  xlab("Date")
