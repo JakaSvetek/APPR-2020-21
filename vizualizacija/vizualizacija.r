2e338da54b947d3133604e876bdd22c1252e6185
Jaka Svetek <jack.svetek@gmail.com>
2020-12-28 20:37:28 +0100

    Naredil funkcijo napredka.

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,11 +1,28 @@
 # 3. faza: Vizualizacija podatkov
 
 require(ggplot2)
+require(data.table)
 
-# Uvozim podatke
+# Funkcija, ki za vsak datum vrne najboljši rezultat
 
-Graf_napredek <- function(Razdalja, Bazen)
-  ggplot(filter(podatki, Distance == Razdalja, Pool == Bazen), aes(x=Swim_date, y=Time)) +
-  geom_line() + 
+Graf_napredek <- function(razdalja, bazen){
+  podatki <- podatki %>% mutate(zaokrozeni_datumi = format(as.Date(Swim_date), "%Y/%m"))
+  disciplina <- filter(podatki, Distance == razdalja, Pool == bazen)
+  disciplina <- disciplina %>% group_by(zaokrozeni_datumi) %>% summarise(Time = min(Time))
+  ggplot(disciplina, aes(x=zaokrozeni_datumi, y=Time)) +
+  geom_point() +
   ylab("Swim Time") + 
   xlab("Date")
+  }
+
+# Funkcija za napredek plavalca
+
+Napredek_plvalca <- function(razdalja, bazen, ime){
+  podatki <- podatki %>% mutate(zaokrozeni_datumi = format(as.Date(Swim_date), "%Y/%m"))
+  disciplina <- filter(podatki, Distance == razdalja, Pool == bazen, Name == ime)
+  disciplina <- disciplina %>% group_by(zaokrozeni_datumi) %>% summarise(Time = min(Time))
+  ggplot(disciplina, aes(x=zaokrozeni_datumi, y=Time)) +
+  geom_point() +
+  ylab("Swim Time") + 
+  xlab("Date")
+}
